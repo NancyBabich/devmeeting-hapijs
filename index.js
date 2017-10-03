6bbd3b5b5ac065030c3c7fed421bec0093c8ccdc
Tomasz Drwięga <tomusdrw@gmail.com>
2017-10-04 00:36:03 +0200

    Serving static files using inert.

--- index.js
+++ index.js
@@ -1,42 +1,37 @@
 'use strict'
 
-// Import hapi
 const Hapi = require('hapi')
-const fs = require('fs')
 
-//4/ Configure the server.
 const server = new Hapi.Server()
 server.connection({
   port: process.env.PORT || 3000
 })
 
-//7/ Manually handle requests and serve files in response.
-server.route([{
-  method: 'GET',
-  path: '/',
-  handler: (request, reply) => {
-    reply(fs.readFileSync('./public/index.html', 'utf8'))
-  }
-}, {
-  method: 'GET',
-  path: '/styles.css',
-  handler: (request, reply) => {
-    reply(fs.readFileSync('./public/styles.css', 'utf8'))
-      .header('Content-type', 'text/css')
-  }
-}, {
-  method: 'GET',
-  path: '/frontend.js',
-  handler: (request, reply) => {
-    reply(fs.readFileSync('./public/frontend.js', 'utf8'))
-  }
-}])
+// Instead of manualy handling static files, we will register `inert` plugin.
+server.register(require('inert'), err => {
+  rethrow(err)
 
-//7/ Start the server
+  //8/ And just create a single route resolving files from `public` directory.
+  server.route([{
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: 'public'
+      }
+    }
+  }])
+})
+
 server.start(err => {
-  if (err) {
-    throw err;
-  }
+  rethrow(err)
 
   console.log(`Server running at ${server.info.port}`)
 })
+
+function rethrow (err) {
+  if (err) {
+    console.error(err)
+    throw err
+  }
+}
